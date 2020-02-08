d26f739fc6c72bb370c4d6850f8d50943ca93a5b
Tim Joosten <Tjoosten3@gmail.com>
2020-02-08 08:19:14 +0100

    Implement truncate all method in the seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,16 +1,38 @@
 <?php
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 
+/**
+ * Class DatabaseSeeder
+ */
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
+    public function run(): void
     {
-        // $this->call(UsersTableSeeder::class);
+        DB::connection()->disableQueryLog();
+        Model::unguard();
+
+        $this->truncateDatabase();
+
+        // Run additional seeders
+    }
+
+    protected function truncateDatabase(): void
+    {
+        Schema::disableForeignKeyConstraints();
+
+        collect(DB::select("SHOW FULL TABLES WHERE Table_Type = 'BASE TABLE'"))
+            ->map(static function (stdClass $tableProperties): string {
+                return get_object_vars($tableProperties)[key($tableProperties)];
+            })->reject(static function (string $tableName): bool {
+                return $tableName === 'migrations';
+            })->each(static function (string $tableName) {
+                DB::table($tableName)->truncate();
+            });
+
+        Schema::enableForeignKeyConstraints();
     }
 }
